06cf6c52480cc23a6706f2aceefdc7d4f1c35074
Illia Sauchuk <ilyas.sporichev@gmail.com>
2019-10-24 11:24:41 +0300

    testing and deployment added

--- tests/Inbox.test.js
+++ tests/Inbox.test.js
@@ -1,3 +1,38 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+const compile = require('../compile');
+const compileInterface = compile.interface;
+const bytecode = compile.bytecode;
+
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(compileInterface))
+        .deploy({ data: bytecode, arguments: ['Keko testo']})
+        .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('has init message', async () => {
+        const message = await inbox.methods.message().call();
+
+        assert.equal(message, 'Keko testo')
+    });
+
+    it('modifies the message', async () => {
+        await inbox.methods.setMessage('well').send({ from: accounts[0], gas: '1000000' });
+        const message = await inbox.methods.message().call();
+
+        assert.equal(message, 'well')
+    });
+});
