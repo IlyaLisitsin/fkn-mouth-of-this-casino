5a2f128c52f25b76e6febcc1ccb8fc03a4dc6ecf
Illia <ilyas.sporichev@gmail.com>
2019-11-08 15:01:20 +0300

    factort instance fix

--- src/bet-factory-instance.js
+++ src/bet-factory-instance.js
@@ -3,10 +3,8 @@ import BetFactory from './contracts-build/BetFactory';
 
 let instance;
 
-console.log('web3', web3)
-
 if (web3) {
-    new web3.eth.Contract(
+    instance = new web3.eth.Contract(
         JSON.parse(BetFactory.interface),
         '0xAB3cEcca29f382Bb1dc00f1cbe7b27faCdCCFaD9',
     );
